698ac225a1b17b8c1e9725899431a0cae54cf1a4
Gabriel <gabriel@gabriel-System-Product-Name>
2023-10-05 15:29:02 -0700

    i forgot

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,4 +5,4 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 add_test(TEST "/home/gabriel/Desktop/C++/upgraded-disco/build/TEST")
-set_tests_properties(TEST PROPERTIES  _BACKTRACE_TRIPLES "/home/gabriel/Desktop/C++/upgraded-disco/CMakeLists.txt;41;add_test;/home/gabriel/Desktop/C++/upgraded-disco/CMakeLists.txt;0;")
+set_tests_properties(TEST PROPERTIES  _BACKTRACE_TRIPLES "/home/gabriel/Desktop/C++/upgraded-disco/CMakeLists.txt;42;add_test;/home/gabriel/Desktop/C++/upgraded-disco/CMakeLists.txt;0;")
