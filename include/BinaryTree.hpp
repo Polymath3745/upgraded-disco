2bf7f290c2fb110fb0dc98c7dbbd3ea118479585
Gabriel <gabriel@gabriel-System-Product-Name>
2023-11-01 16:04:16 -0700

    declared new function

--- include/BinaryTree.hpp
+++ include/BinaryTree.hpp
@@ -42,6 +42,13 @@ class BinaryTree
         // Level order Traversal
         void levelOrderTraversal();
 
+        /**
+         * @brief Get the Height of the BST
+         * 
+         * @return int 
+         */
+        int getHeight();
+
     private:
         TreeNode* root;
 
