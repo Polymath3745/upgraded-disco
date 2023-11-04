638ead9bb1ae3df35edb24010c686e4a0d3e92b5
Gabriel <gabriel@gabriel-System-Product-Name>
2023-11-03 17:09:03 -0700

    tested isBalanced on an unbalanced tree

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,8 @@ int main()
     BinaryTree tree;
 
     // Insert elements into the tree
-    tree.insert(10);
+    // balanced tree
+/*     tree.insert(10);
     tree.insert(5);
     tree.insert(3);
     tree.insert(8);
@@ -19,8 +20,15 @@ int main()
     tree.insert(15);
     tree.insert(12);
     tree.insert(11);
-    tree.insert(18);
+    tree.insert(18); */
     
+    // unbalanced tree
+    tree.insert(5);
+    tree.insert(3);
+    tree.insert(8);
+    tree.insert(2);
+    tree.insert(9);
+    tree.insert(1);
     
 
     bool result = tree.isBalanced();
